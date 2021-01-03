9bbf0de969d48c8a2381e571eeee9bc5fe790be1
Justin Talictic <jaytalictic@gmail.com>
2021-01-03 10:22:55 -0600

    User authentication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 // import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -23,7 +24,8 @@ const Register = ({ setAlert }) => {
         if (password !== password2) {
             setAlert('Passwords do not match', 'danger');
         } else {
-            /*  Able to access backend 
+            register({ name, email, password });
+            /*  Able to access backend using axios
             const newUser = {
                 name,
                 email,
@@ -48,6 +50,11 @@ const Register = ({ setAlert }) => {
         }
     };
 
+    //Redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />;
+    }
+
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
@@ -62,7 +69,6 @@ const Register = ({ setAlert }) => {
                         name="name"
                         value={name}
                         onChange={e => onChange(e)}
-                        required
                     />
                 </div>
                 <div className="form-group">
@@ -72,7 +78,6 @@ const Register = ({ setAlert }) => {
                         name="email"
                         value={email}
                         onChange={e => onChange(e)}
-                        required
                     />
                     <small className="form-text">
                         This site uses Gravatar so if you want a profile image,
@@ -86,7 +91,6 @@ const Register = ({ setAlert }) => {
                         name="password"
                         value={password}
                         onChange={e => onChange(e)}
-                        minLength="6"
                     />
                 </div>
                 <div className="form-group">
@@ -96,7 +100,6 @@ const Register = ({ setAlert }) => {
                         name="password2"
                         value={password2}
                         onChange={e => onChange(e)}
-                        minLength="6"
                     />
                 </div>
                 <input
@@ -114,6 +117,12 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
