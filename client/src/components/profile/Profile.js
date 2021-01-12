11c66af5df3d5db59f28791638b7ed635a053c5a
Justin Talictic <jaytalictic@gmail.com>
2021-01-11 23:36:32 -0600

    Profile components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -31,6 +34,25 @@ const Profile = ({
                                 Edit Profile
                             </Link>
                         )}
+                    <div class="profile-grid my-1">
+                        <ProfileTop profile={profile} />
+                        <ProfileAbout profile={profile} />
+                        <div className="profile-exp bg-white p-2">
+                            <h2 className="text-primary">Experience</h2>
+                            {profile.experience.length > 0 ? (
+                                <Fragment>
+                                    {profile.experience.map(experience => (
+                                        <ProfileExperience
+                                            key={experience._id}
+                                            experience={experience}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>Np experience credentials</h4>
+                            )}
+                        </div>
+                    </div>
                 </Fragment>
             )}
         </Fragment>
